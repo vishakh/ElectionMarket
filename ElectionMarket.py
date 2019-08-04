a254e908986c4352a9bcf5ddc77b564dd6b54734
Vishakh <vishakh@users.noreply.github.com>
2019-08-04 00:21:16 -0400

    Fixed syntax.

--- ElectionMarket.py
+++ ElectionMarket.py
@@ -13,26 +13,42 @@ class ElectionMarket(sp.Contract):
         sp.if ~(params.candidate in self.data.balances):
             self.data.balances[params.candidate] = sp.Map()
             self.data.balances[params.candidate][self.data.administrator] = params.amount
+            
+    @sp.entryPoint
+    def addUser(self, params):
+        sp.if ~("credits" in self.data.balances):
+            self.data.balances["credits"] = sp.Map()
+        sp.if ~(sp.sender in self.data.balances["credits"]):
+            self.data.balances["credits"][sp.sender] = 100
 
     # candidate - Which candidate's tokens to buy
     # amount    - Amount of tokens to buy
     @sp.entryPoint
     def buy(self, params):
+        # The given candidate should be valid
+        sp.verify(params.candidate in self.data.balances)
+        # The buyer should have enough credits
+        sp.verify(self.data.balances["credits"][sp.sender] >= params.amount)
+        # There should be enough free tokens for the given candidate
         sp.verify(self.data.balances[params.candidate][self.data.administrator] >= params.amount)
+        # Ensure the sender starts out with a zero token balance if they have never owned this candidate's tokens.
         sp.if ~(sp.sender in self.data.balances[params.candidate]):
             self.data.balances[params.candidate][sp.sender] = 0
         self.data.balances[params.candidate][sp.sender] += params.amount
         self.data.balances[params.candidate][self.data.administrator] -= params.amount
+        self.data.balances["credits"][sp.sender] -= params.amount 
     
     # candidate - Which candidate's tokens to buy
     # amount    - Amount of tokens to buy
     @sp.entryPoint
     def sell(self, params):
+        # The given candidate should be valid
+        sp.verify(params.candidate in self.data.balances)
         sp.verify(self.data.balances[params.candidate][sp.sender] >= params.amount)
         self.data.balances[params.candidate][sp.sender] -= params.amount
         self.data.balances[params.candidate][self.data.administrator] += params.amount
-        
-if "templates" not in __name__:
+        self.data.balances["credits"][sp.sender] += params.amount
+
     @addTest(name = "ElectionMarket test")
     def test():
         admin = sp.address("admin")
@@ -40,6 +56,8 @@ if "templates" not in __name__:
         user2 = sp.address("monkey")
         c1 = ElectionMarket(admin)
         html  = c1.fullHtml()
+        html += c1.addUser().run(sender = user1).html()
+        html += c1.addUser().run(sender = user2).html()
         html += c1.addCandidate(candidate="Bernie Sanders", amount = 1000).run(sender = admin).html()
         html += c1.addCandidate(candidate="Elizabeth Warren", amount = 1000).run(sender = admin).html()
         html += c1.buy(candidate="Bernie Sanders", amount = 100).run(sender = user1).html()
